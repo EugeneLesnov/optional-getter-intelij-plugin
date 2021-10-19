b462a436820d3ddadfa485b35a2e8eaec96efab2
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-19 21:42:57 +0300

    feat: changed concept to the implementation of StartupActivity

---
+++ src/main/java/com/github/eugenelesnov/OptionalGetterActivity.java
@@ -0,0 +1,13 @@
+package com.github.eugenelesnov;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import org.jetbrains.annotations.NotNull;
+
+public class OptionalGetterActivity implements StartupActivity {
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+
+    }
+}
