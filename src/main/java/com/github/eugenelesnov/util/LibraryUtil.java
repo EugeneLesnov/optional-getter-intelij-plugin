a023d435d0445ba1ac5a493489cadae2505973a8
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-19 22:44:54 +0300

    refactor: changed detecting the library in project

--- src/main/java/com/github/eugenelesnov/util/LibraryUtil.java
+++ src/main/java/com/github/eugenelesnov/util/LibraryUtil.java
@@ -4,7 +4,8 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiPackage;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.CachedValueProvider;
 import com.intellij.psi.util.CachedValuesManager;
 import org.jetbrains.annotations.NotNull;
@@ -13,11 +14,12 @@ public class LibraryUtil {
 
     public static final String OPTIONAL_GETTER_ANNOTATION_CLASS = "com.github.eugenelesnov.OptionalGetter";
 
-    public static boolean hasLibrary(@NotNull Project project) {
+    public static boolean hasLibrary(@NotNull Project project, @NotNull JavaPsiFacade javaPsiFacade) {
         ApplicationManager.getApplication().assertReadAccessAllowed();
 
         return CachedValuesManager.getManager(project).getCachedValue(project, () -> {
-            PsiPackage aPackage = JavaPsiFacade.getInstance(project).findPackage(OPTIONAL_GETTER_ANNOTATION_CLASS);
+            PsiClass aPackage = javaPsiFacade
+                    .findClass(OPTIONAL_GETTER_ANNOTATION_CLASS, GlobalSearchScope.allScope(project));
             return new CachedValueProvider.Result<>(aPackage, ProjectRootManager.getInstance(project));
         }) != null;
     }
